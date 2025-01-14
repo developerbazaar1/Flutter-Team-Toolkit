0374ba134be2f2f11c7622057c6970e8d906e0a2
dbTushar <dbtechtushar@gmail.com>
2025-01-14 10:44:08 +0530

    Blocking the screen - Tushar

---
+++ lib/blocking_screens/usage/usage_file.dart
@@ -0,0 +1,40 @@
+/// Find the usage of block screen
+/// Commenting the code because it is an example of usage
+
+
+// import 'package:flutter/material.dart';
+//
+/// Make the class Stateful
+///
+// class UsageFile extends StatefulWidget {
+//   const UsageFile({super.key});
+//
+//   @override
+//   State<UsageFile> createState() => _UsageFileState();
+// }
+//
+// class _UsageFileState extends State<UsageFile> {
+//
+//   @override
+//   Widget build(BuildContext context) {
+/// Use Media Query for responsiveness
+
+//     // final width = MediaQuery.of(context).size.width;
+//     // final height = MediaQuery.of(context).size.height;
+//
+/// Use Obx on the Scaffold
+//     return Obx(
+//           () => BlockingScreen(
+//         isBlocking: controller.isSaved.value, // Pass the blocking flag
+//         child: Scaffold(
+//           // appBar: AnyAppBar,
+//           // body: Column(
+//           //   child : (
+//           //   // Your Body
+//           //   )
+//           // ),
+//         ),
+//       ),
+//     );
+//   }
+// }
